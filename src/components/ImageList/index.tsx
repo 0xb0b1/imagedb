0ef961d865784dbe300b7d31b82740c000021e9f
0xb0b1 <vcente82@gmail.com>
2023-04-17 14:51:12 -0400

    style: responsive grid for mobile

--- src/components/ImageList/index.tsx
+++ src/components/ImageList/index.tsx
@@ -15,7 +15,7 @@ interface ImagesProps {
 
 export const ImageList = ({ images }: ImagesProps) => {
   return (
-    <div className='grid grid-cols-2 md:grid-cols-3 gap-4'>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
       {images.map((image) => (
         <ImageCard key={image.id} data={image} />
       ))}
